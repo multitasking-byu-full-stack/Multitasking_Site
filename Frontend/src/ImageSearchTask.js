7153151ecd1fcc8a38445b9155fca04a664f8ffd
Jonathan Wilding <jonathan.r.wilding@gmail.com>
2021-02-25 11:46:18 -0700

    stash

--- Frontend/src/ImageSearchTask.js
+++ Frontend/src/ImageSearchTask.js
@@ -56,53 +56,53 @@ class ImageCardComponent extends React.Component {
     return JSON.stringify(this.state.responses);
   }
 
- 
 
-render() {
-  if (this.lImages.length === 0) {
-    this.postData();
+
+  render() {
+    if (this.lImages.length === 0) {
+      this.postData();
+      return (
+        <Redirect to='/thank-you' />
+      );
+    }
+    const currentDate = new Date();
+    const in5minutes = new Date(currentDate.getTime() + 5 * 60000);
     return (
-      <Redirect to='/thank-you' />
-    );
-  }
-  const currentDate = new Date();
-  const in5minutes = new Date(currentDate.getTime() + 5 * 60000);
-  return (
-    <div className="container">
-      <ProgressBar animated variant="success" now={this.i / this.n * 100} />
-      <br>
-      </br>
-      <div className="row">
+      <div className="container">
+        <ProgressBar animated variant="success" now={this.i / this.n * 100} />
+        <br>
+        </br>
+        <div className="row">
 
-        <div className="col-md-8">
-          <div className="card text-center">
-            <div className='card-header'>Embeded React App</div>
-            <div className='img-container'>
-              <img className='card-img-top' src={`./images/TSA_bw/File ${this.lImages[0]}.jpg`} alt="Maybe Suspicious" />
-            </div>
-            <div className='card-footer text-right'>
-              <NextButtons onClick={status => this.handleClick(status)} />
+          <div className="col-md-8">
+            <div className="card text-center">
+              <div className='card-header'>Embeded React App</div>
+              <div className='img-container'>
+                <img className='card-img-top' src={`./images/TSA_bw/File ${this.lImages[0]}.jpg`} alt="Maybe Suspicious" />
+              </div>
+              <div className='card-footer text-right'>
+                <NextButtons onClick={status => this.handleClick(status)} />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card">
-            <div className="card-header">
-              <h4 style={{ textAlign: 'center' }}><Countdown date={in5minutes} /></h4>
-              {/* Task Description</h4> */}
-            </div>
-            <div className="card-body">
-              <p>You are a security guard for the Transportation Security Administration. <b>Your job is to scan the x-rays of passengers' bags for guns or knives.</b> If the bag contains a
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-header">
+                <h4 style={{ textAlign: 'center' }}><Countdown date={in5minutes} /></h4>
+                {/* Task Description</h4> */}
+              </div>
+              <div className="card-body">
+                <p>You are a security guard for the Transportation Security Administration. <b>Your job is to scan the x-rays of passengers' bags for guns or knives.</b> If the bag contains a
                       gun or a knife, click "Contraband Present." All other bags will "No Contraband."</p>
-              <p><b>Most bags will not have suspicious items in them.</b> You will have ten seconds per image to select an option before it automatically advances. This demo contains five
+                <p><b>Most bags will not have suspicious items in them.</b> You will have ten seconds per image to select an option before it automatically advances. This demo contains five
                       example images. The real task will have many more and will require you to remain attentive for up to 10 minutes. Do your best!</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default ImageCardComponent;
